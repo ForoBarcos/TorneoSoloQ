8aeeb2eed36708ee7a10f011be1619f5adade981
ForoBarcos <66043195+ForoBarcos@users.noreply.github.com>
2020-05-31 05:56:09 +0200

    Add files via upload

---
+++ sendbymail.php
@@ -0,0 +1,24 @@
+<?php 
+error_reporting(0); 
+$nombre = $_POST['nombre']; 
+$mail= $_POST['email']; 
+$procedencia=$_POST['procedencia']; 
+$producto=$_POST['producto']; 
+$cantidad=$_POST['cantidad']; 
+/*$header = 'From: ' . $mail . ", de la poblacion ".$poblacion."\r\n"; */
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
+$header .= "Mime-Version: 1.0 \r\n"; 
+$header .= "Content-Type: text/plain"; 
+
+$mensaje = "Este mensaje fue enviado por " . $_POST['nombre'] . " \r\n"; 
+$mensaje .= "Su e-mail es: " . $_POST['email'] . " \r\n";
+$mensaje .= "Procedencia: " . $_POST['procedencia'] . " \r\n"; 
+$mensaje .="El producto que busca: ".$_POST['producto'] . " \r\n"; 
+$mensaje .= "Cantidad: " . $_POST['cantidad'] . " \r\n"; 
+$mensaje .= "Enviado el " . date('d/m/Y', time()); 
+
+$para = 'asirgames97@gmail.com'; 
+$asunto = 'Quiniela SoloQ Challenge'; 
+
+mail($para, $asunto, utf8_decode($mensaje), $header); 
+?>
